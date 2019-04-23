e6539513a09d0ade8fae95b20324779ee3843d1f
Leijurv <leijurv@gmail.com>
2019-04-22 18:43:26 -0700

    check all signatures

---
+++ src/main/java/io/github/ImpactDevelopment/installer/GithubReleases.java
@@ -0,0 +1,35 @@
+package io.github.ImpactDevelopment.installer;
+
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class GithubReleases {
+
+    public static GithubRelease[] getReleases(String repo) throws IOException {
+        return Installer.gson.fromJson(new InputStreamReader(new URL("https://api.github.com/repos/" + repo + "/releases").openStream()), GithubRelease[].class);
+    }
+
+    public class GithubRelease {
+        public String tag_name;
+        public ReleaseAsset[] assets;
+    }
+
+    public class ReleaseAsset {
+        public String name;
+        public String browser_download_url;
+
+        private String data;
+
+        public String fetch() throws IOException {
+            if (data == null) {
+                data = IOUtils.toString(new URL(browser_download_url).openStream(), StandardCharsets.UTF_8);
+            }
+            return data;
+        }
+    }
+}
