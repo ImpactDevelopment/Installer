03864b378f1a9f04be336f838a13825ae6e238b0
LeafHacker <leafhacker@yahoo.com>
2020-04-28 03:35:51 +0100

    MavenResolver parse coordinates properly
    Added a small Artifact class that takes maven coords and provides methods to get the various parts. This makes it easier for MavenResolver to extract those.
    
    Followed the gradle-style maven coordinates which are slightly different from the aether coordinates used internally by maven.
    
    This is a precursor to some upcoming MultiMC memes...

--- src/main/java/io/github/ImpactDevelopment/installer/optifine/OptiFine.java
+++ src/main/java/io/github/ImpactDevelopment/installer/optifine/OptiFine.java
@@ -22,6 +22,8 @@
 
 package io.github.ImpactDevelopment.installer.optifine;
 
+import io.github.ImpactDevelopment.installer.libraries.MavenResolver;
+
 import javax.annotation.Nullable;
 import java.io.*;
 import java.lang.reflect.InvocationTargetException;
@@ -31,7 +33,6 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.regex.Matcher;
@@ -165,12 +166,12 @@ public class OptiFine {
     // Install optifine jar and launchwrapper (if required) to the target libraries directory
     public void install(Path libs, Path vanilla) throws IOException {
         try {
-            installOptiFine(libs.resolve(pathFromID(getOptiFineID())), vanilla);
+            installOptiFine(libs.resolve(MavenResolver.getPath(getOptiFineID())), vanilla);
         } catch (InvocationTargetException | IllegalAccessException e) {
             throw new RuntimeException("Error calling OptiFine patcher method", e);
         }
         if (getLaunchwrapperID() != null) {
-            installLaunchwrapper(libs.resolve(pathFromID(getLaunchwrapperID())));
+            installLaunchwrapper(libs.resolve(MavenResolver.getPath(getLaunchwrapperID())));
         }
     }
 
@@ -194,18 +195,5 @@ public class OptiFine {
                 Files.copy(input, destination, REPLACE_EXISTING);
             }
         }
-    }
-
-    // Get a maven path based on an artifact id.
-    // Ignores any classifier since (last I checked) so does the Minecraft Launcher
-    private static Path pathFromID(String artifact) throws IllegalArgumentException {
-        String[] parts = artifact.split(":");
-        if (parts.length < 3) {
-            throw new IllegalArgumentException("OptiFine.pathFromID() expected an artifact id with at least three parts, got " + artifact);
-        }
-        String group = parts[0].replace(".", File.separator);
-        String id = parts[1];
-        String version = parts[2];
-        return Paths.get(group).resolve(id).resolve(version).resolve(String.format("%s-%s.jar", id, version));
     }
 }
--- src/main/java/io/github/ImpactDevelopment/installer/target/targets/Vanilla.java
+++ src/main/java/io/github/ImpactDevelopment/installer/target/targets/Vanilla.java
@@ -159,7 +159,7 @@ public class Vanilla implements InstallationMode {
             {
                 JsonObject artifact = new JsonObject();
                 downloads.add("artifact", artifact);
-                artifact.addProperty("path", MavenResolver.partsToPath(lib.getName().split(":")));
+                artifact.addProperty("path", MavenResolver.getPath(lib.getName()));
                 artifact.addProperty("sha1", lib.getSHA1());
                 artifact.addProperty("size", lib.getSize());
                 artifact.addProperty("url", lib.getURL());
